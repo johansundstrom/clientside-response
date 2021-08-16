fa297a4de131ed465a185fc2558d3a58975e5e3d
johansundstrom <johan.sundstrom@mdh.se>
2021-08-16 18:54:56 +0200

    rensat

--- index.js
+++ index.js
@@ -7,13 +7,12 @@ app.use(express.static('public'));
 app.use(express.json({limit: '10kb'})); //http://expressjs.com/en/5x/api.html#express.json
 
 app.post('/api', (request, response) => {
-  console.log(request.body);
   const data = request.body;
-  const timestamp = Date.now();
-  data.timestamp = timestamp;
+  console.log(data);
+
   response.json({
-    status: 'success',
-    timestamp: data.timestamp,
+    cTimestamp: data.cTimestamp,
+    response: 'success',
     first: data.first,
     last: data.last,
     age: data.age
