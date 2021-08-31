a80730dcafd46316645ea854007b174bd569ca3f
Johan sundstrom <johansundstrom@users.noreply.github.com>
2021-08-31 18:40:46 +0200

    Update index.js
    new

--- index.js
+++ index.js
@@ -1,24 +1,42 @@
-const { request } = require('express');
-let express = require('express');
-let port = process.env.PORT || 3000;
-let app = express();
+/*------------------------------------
+documentation: http://expressjs.com/en/5x/api.html
+1. setup
+2. definiera inkommande JSON data
+3. serve static assets from 'public'
+4. Routes HTTP POST requests to the specified path och callback
+5. Binds and listens for connections on the specified host and port
+------------------------------------*/
 
+//setup
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+//definiera inkommande JSON data
+const options = {
+  limit: '10kb', 
+  type: 'application/json'
+};
+app.use(express.json(options));
+
+//serve static assets from 'public'
 app.use(express.static('public'));
-app.use(express.json({limit: '10kb'})); //http://expressjs.com/en/5x/api.html#express.json
 
+//Routes HTTP POST requests to the specified path och callback
 app.post('/api', (request, response) => {
   const data = request.body;
   console.log(data);
 
+  //Sends a JSON response
   response.json({
-    cTimestamp: data.cTimestamp,
     response: 'success',
-    first: data.first,
-    last: data.last,
-    age: data.age
+    timestamp: data.timestamp,
+    lon: data.lon,
+    lat: data.lat
   });
 });
 
+//Binds and listens for connections on the specified host and port
 app.listen(port, function() {
  console.log('App listening on port: ' + port);
 });
